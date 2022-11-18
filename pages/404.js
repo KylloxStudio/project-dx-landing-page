414b1181c9d6a14c29bbb1725f3a96a31ecba4da
Kyllox <kyllox4804@gmail.com>
2022-11-18 10:37:37 +0900

    New Version Compatibility Fixes

--- pages/404.js
+++ pages/404.js
@@ -7,7 +7,7 @@ export default function PageNotFound() {
       <div className="container">
         <p className="error-code">404: Page Not Found</p>
         <p className="error-text">This page could not be found.<br/>The page has been deleted or you've entered a URL that doesn't exist on this site.</p>
-        <Link href="../"><a className="back-link">&#x003C; Return to main page</a></Link>
+        <Link href="../" className="back-link">&#x003C; Return to main page</Link>
       </div>
     </div>
   );
